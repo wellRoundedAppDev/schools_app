57c8b72677ac09c0db9e429277d63b29c57e22f2
PC <wellroundedappdev@gmail.com>
2022-05-22 06:01:12 +0200

    3

--- lib/model/subjects_data.dart
+++ lib/model/subjects_data.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:school_app/api/subjects_api.dart';
 import 'package:school_app/model/subject_model.dart';
@@ -26,8 +27,14 @@ class SubjectsData extends ChangeNotifier{
 
   Subject getSelectedSubject(){
 
-    return subjects[selectedSubjectId - 1];
+    for(int i = 0; i < subjects.length; i++){
 
+      if(subjects[i].id == selectedSubjectId){
+        return subjects[i];
+
+      }
+    }
+      throw 'error';
   }
 
  // Future<List<String>> getSubjectNames() async{
--- lib/widgets/courses_list.dart
+++ lib/widgets/courses_list.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../model/subjects_data.dart';
 
 class CoursesList extends StatelessWidget {
-   CoursesList({Key? key}) : super(key: key);
+  CoursesList({Key? key}) : super(key: key);
 
   var isWeb = kIsWeb;
 
@@ -15,22 +16,36 @@ class CoursesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("List of courses"),
-            SizedBox(height: 5),
-            Expanded(
-              child: ListView.builder(shrinkWrap:true,itemBuilder: (context, index){
-                final teacherName = Provider.of<SubjectsData>(context).getSelectedSubject().courses[index].teacher_name;
-                final startingDate = Provider.of<SubjectsData>(context).getSelectedSubject().courses[index].starting_date;
-                return Container(margin: EdgeInsets.only(bottom: 5),child: ListTile(tileColor: Colors.white ,leading: Icon(Icons.school),title: Text(teacherName), subtitle: Text(startingDate)));
-              },
-                itemCount: Provider.of<SubjectsData>(context).getSelectedSubject().courses.length,
-              ),
-            )
-          ]
-      ),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(child: Text("List of courses")),
+        Container(color: Colors.grey[200], height: 5),
+        Expanded(
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final teacherName = Provider.of<SubjectsData>(context)
+                  .getSelectedSubject()
+                  .courses[index]
+                  .teacher_name;
+              final startingDate = Provider.of<SubjectsData>(context)
+                  .getSelectedSubject()
+                  .courses[index]
+                  .starting_date;
+              return Container(
+                  margin: EdgeInsets.only(bottom: 5),
+                  child: ListTile(
+                      tileColor: Colors.white,
+                      leading: Icon(Icons.school),
+                      title: Text(teacherName),
+                      subtitle: Text(startingDate)));
+            },
+            itemCount: Provider.of<SubjectsData>(context)
+                .getSelectedSubject()
+                .courses
+                .length,
+          ),
+        )
+      ]),
     );
   }
 }
